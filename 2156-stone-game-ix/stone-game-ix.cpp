3d8e7730e1f631d2b3fc6ede1675849c4416e785
Arpit Yadav <arpityadav639338@gmail.com>
2026-08-16 21:55:54 +0530

    Time: 4 ms (62.44%) | Memory: 131.2 MB (69.29%) - LeetSync

---
+++ 2156-stone-game-ix/stone-game-ix.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool stoneGameIX(vector<int>& stones) {
+         int count0 = 0;
+        int count1 = 0;
+        int count2 = 0;
+
+        for (int ele : stones) {
+
+            if (ele % 3 == 0) {
+                count0++;
+            }
+            else if (ele % 3 == 1) {
+                count1++;
+            }
+            else {
+                count2++;
+            }
+        }
+
+        // If count of remainder 0 stones is even
+        if (count0 % 2 == 0) {
+
+            return count1 >= 1 && count2 >= 1;
+        }
+
+        // If count of remainder 0 stones is odd
+        return abs(count1 - count2) > 2;
+    }
+};
