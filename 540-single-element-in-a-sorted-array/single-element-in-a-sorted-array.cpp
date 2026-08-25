4a52789e1219f4fdb2fdab0b922e634ca617d6b1
Arpit Yadav <arpityadav639338@gmail.com>
2026-08-25 23:58:52 +0530

    Time: 15 ms (3.50%) | Memory: 31.1 MB (7.42%) - LeetSync

---
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+
+    int singleNonDuplicate(vector<int>& nums) {
+       unordered_map<int, int> mp;
+
+        for(int num : nums) {
+            mp[num]++;
+        }
+
+        for(auto p : mp) {
+            if(p.second == 1) {
+                return p.first;
+            }
+        }
+
+        return 0;
+    }
+};
